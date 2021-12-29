c27057c60bb30265bc1d0eb53e9ffff8534d8559
Irineu333 <=>
2021-12-29 00:06:52 -0300

    chore : change environment to production

--- app/src/main/java/com/neo/fbrules/core/Environment.kt
+++ app/src/main/java/com/neo/fbrules/core/Environment.kt
@@ -3,5 +3,5 @@ package com.neo.fbrules.core
 object Environment {
     private const val DEVELOPMENT = "development"
     private const val PRODUCTION = "production"
-    const val FIREBASE = DEVELOPMENT
+    const val FIREBASE = PRODUCTION
 }
